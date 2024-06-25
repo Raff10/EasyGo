cd40cbb44a4f38b9fdbbacd4e994bd636c421db1
Raff10 <38460287+Raff10@users.noreply.github.com>
2024-06-26 08:15:06 +1000

    Update cypress.config.js
    Added URL for API test

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,8 @@ const cucumber = require("cypress-cucumber-preprocessor").default;
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://www.saucedemo.com/',
+    baseUrl: 'https://www.saucedemo.com/', //for UI
+    "URL": "https://gorest.co.in/public/v2/users/", //for API
     chromeWebSecurity: false,
     specPattern: "cypress/e2e/features/**/*.feature",
     setupNodeEvents(on, config) {
